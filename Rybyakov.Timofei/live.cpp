689f7b78e497d77069fd87f5ede18aec5545f0d9
Nemopoll <tim.rybyakov1@gmail.com>
2026-09-17 12:13:36 +0300

    Add files via upload

---
+++ Rybyakov.Timofei/live.cpp
@@ -0,0 +1,70 @@
+﻿#include <stdio.h>
+
+
+using RUB = int;
+
+
+struct Person {
+	RUB cash;
+	RUB salary;
+};
+
+struct Person rybka;
+
+
+void rybka_salary(const int year, const int month)
+{
+	if (year == 2026 and month == 11) { //Promotion
+		rybka.salary = 60.000;
+	}
+
+
+	rybka.cash += rybka.salary;
+}
+
+
+void rybka_print()
+{
+	printf("Rybka cash = %d\n", rybka.cash);
+}
+
+
+void rybka_init()
+{
+	rybka.cash = 10.000;
+	rybka.salary = 60.000;
+}
+
+
+void simulation()
+
+{
+	int year = 2026;
+	int month = 9;
+	while (not(year == 2027 and month == 9)) {
+
+		rybka_salary(year, month);
+		// rybka_public utillities();
+		// rybka_parking();
+		// rybka_food();
+		// rybka_entertaintment();
+		// rybka_
+
+
+		++month;
+		if (month == 13) {
+			++year;
+			month = 1;
+		}
+		
+	}
+}
+
+int main()
+{
+	rybka_init();
+
+	simulation();
+	
+	rybka_print();
+}
